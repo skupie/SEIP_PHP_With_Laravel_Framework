a88404af6196194a5baebe17be070bd2d478b548
skupie <114020227+skupie@users.noreply.github.com>
2023-05-17 10:37:35 +0600

    Update Home.php
    print Array using foreach and default function ( var_dump & print_r)

--- PHP/DAY-27/app/classes/Home.php
+++ PHP/DAY-27/app/classes/Home.php
@@ -13,22 +13,31 @@ class Home
         $this->students = [
             0 => [
                 'id'=>1,
-                'name'=>'Yeasin ',
+                'name'=>'Yeasin',
                 'email'=>'yeasin@gmail.com',
-                'contact'=>'0168xxxxx99 ',
+                'contact'=>[
+                    'personal'=>'0168xxxxx99',
+                    'parent'=>'0181xxxxx42'
+                ],
                 'img'=>'1.jpg',
             ],
             1 =>[
                 'id'=>2,
-                'name'=>'Shakil ',
+                'name'=>'Shakil',
                 'email'=>'Shakil@gmail.com',
-                'contact'=>'0167xxxxx58 ',
+                'contact'=>[
+                    'personal'=>'0167xxxxx58',
+                    'parent'=>'0171xxxxx42'
+                ],
                 'img'=>'2.jpg'
             ],
             2=> ['id'=>3,
                 'name'=>'Mustakim ',
                 'email'=>'Mustakim@gmail.com',
-                'contact'=>'0140xxxxx96 ',
+                'contact'=>[
+                    'personal'=>'0140xxxxx96',
+                    'parent'=>'0155xxxxx42'
+                ],
                 'img'=>'3.jpg'
             ],
         ];
@@ -36,12 +45,31 @@ class Home
 
     public function index(){
         //printing value from the array key:value pair
-        foreach ($this->students as $index => $student){
 
-            foreach ($student as $item){
-                echo $item;
+        echo "Array value using Foreach Loop: ". "<br/>";
+
+        foreach ($this->students as $student){
+
+            foreach ($student as $key=> $item){
+                if(is_array($item)){
+                    foreach ($item as $value){
+                        echo $value.' ';
+                    }
+                }
+                else
+                    echo $item.' ';
             }
+            echo "<br/>"."<br/ >";
+
+            echo "Using Print_r builtin function: "."<br/>";
+            echo '<pre>';
+            print_r($this->students);
+
             echo "<br/>";
+            echo "Using var_dump builtin function: "." <br/>"."<br/>";
+            var_dump($this->students);
+
+
 //            echo $student['name'].' '.$student['email'].' '.$student['contact'];
         }
     }
